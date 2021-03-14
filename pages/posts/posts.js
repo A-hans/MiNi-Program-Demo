502d4d33ebda634a7c8cbe10d9c8480e56433ec8
LiZhengHan <lzh821128@live.com>
2021-03-14 17:42:51 +0800

    03-14

--- pages/posts/posts.js
+++ pages/posts/posts.js
@@ -5,14 +5,25 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    a:"2020LP夏季赛季后赛观赛指南"
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // let a = "2020LP夏季赛季后赛观赛指南";
+    // console.log(a);
+    let content ={
+      title:"2020LPL夏季赛季后赛观赛指南",
+      image:"/images/lpl.png",
+      content:"英雄联盟赛事官网,LPL职业联赛,全球总决赛,季中冠军赛,德玛西亚杯,国内外赛事,丰富的英雄联盟赛事资讯与各类大型赛事直播尽在英雄联盟赛事官方网站",
+      views:102,
+      likes:92,
+      avatar:"/images/avatar/3.png",
+      date:"Nov 18 2020"
+    }
+    this.setData(content)
   },
 
   /**
